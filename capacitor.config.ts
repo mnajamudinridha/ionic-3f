4e92f57c0d3eeade731aeba0eb6b8ccd72618799
M. Najamudin Ridha <najamudin@politala.ac.id>
2023-11-13 09:34:23 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-api',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
