bd9c888956f1e37df4aeab5c95fc1f191b210afa
M. Najamudin Ridha <najamudin@politala.ac.id>
2023-12-16 15:45:48 +0800

    tambah loading spinner

--- src/app/pages/tutorial-list/tutorial-list.page.ts
+++ src/app/pages/tutorial-list/tutorial-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 import { TutorialService } from 'src/app/services/tutorial.service';
 
 @Component({
@@ -11,12 +12,24 @@ export class TutorialListPage implements OnInit {
   tutorials : any[] = [];
   foto: string = 'https://source.unsplash.com/random/50×50';
 
-  constructor(private tutorialService : TutorialService) { }
+  constructor(private tutorialService : TutorialService,
+              private loadingController : LoadingController) { }
 
   ngOnInit() {
+    this.loadingData();
+  }
+
+  async loadingData(){
+    const loading = await this.loadingController.create({
+      message:'Loading ...',
+      spinner:'crescent'
+    });
+    await loading.present();
+    
     this.tutorialService.getTutorial().subscribe((res) => {
       this.tutorials.push(...res.Hasil);
       console.log(this.tutorials);
+      loading.dismiss();
     });
   }
 
